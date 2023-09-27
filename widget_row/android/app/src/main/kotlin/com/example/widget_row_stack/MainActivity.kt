d16abc24b25bcfe883c7e5538032ee7e464ccc46
Harry Setya Hadi <xmoensen@gmail.com>
2023-09-28 00:51:16 +0700

    OK
    OK

--- widget_row/android/app/src/main/kotlin/com/example/widget_row_stack/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.widget_row_stack
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ widgte_row/android/app/src/main/kotlin/com/example/widgte_row/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widgte_row
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
